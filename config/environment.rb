d82447ddd67b5b580299ad82ec315a18da536511
Juan Peon <nemesys101@gmail.com>
2015-04-02 12:15:00 -0300

    devise action_mailer host config

--- config/environment.rb
+++ config/environment.rb
@@ -4,16 +4,13 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Bootprod::Application.initialize!
 
-ActionMailer::Base.delivery_method = :smtp
-
-mail_options = {
-	:address    => "smtp.mandrillapp.com",
-	:port       => 587,
-	:user_name  => ENV["MANDRILL_USERNAME"],
-	:password   => ENV["MANDRILL_PASSWORD"],
-	:enable_ssl => true
+ActionMailer::Base.smtp_settings = {
+  :port           => 587,
+  :address        => "smtp.mandrillapp.com",
+  :user_name      => ENV["MANDRILL_USERNAME"],
+  :password       => ENV["MANDRILL_PASSWORD"],
+  :domain         =>'heroku.com',
+  :authentication => :plain
 }
 
-Mail.defaults do
-	delivery_method :smtp, mail_options
-end
+ActionMailer::Base.delivery_method = :smtp
